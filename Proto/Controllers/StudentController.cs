ce0baaa0715caf76e1db77b0011b082679ac1b69
K.Gonzalez <kathrynn.gonzalez@gmail.com>
2014-11-13 15:59:10 -0800

    Added controllers for the Users to be forwarded to their own pages after login. Will add views later

---
+++ Proto/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proto.Controllers
+{
+    public class StudentController : Controller
+    {
+        //
+        // GET: /Student/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
